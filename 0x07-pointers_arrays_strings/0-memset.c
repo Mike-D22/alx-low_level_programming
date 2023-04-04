6877d859d7067abdf2bfc135392d9077ad5bbc86
Mike-D22 <qamp26@gmail.com>
2023-04-03 20:14:46 -0700

    memset; git commit -m memset git commit -m 'memset; git commit -m 'memset' git push
    ;
    clear
    git add .
    git commit -m 'memset;
    git commit -m 'memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,14 +7,14 @@
  *
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *g, char z, unsigned int n)
 {
 	int i = 0;
 
 	for (; n > 0; i++)
 	{
-		s[i] = b;
+		g[i] = z;
 		n--;
 	}
-	return (s);
+	return (g);
 }
