5183c162edf46e46648a02283a5fd825daee33bd
Mike-D22 <qamp26@gmail.com>
2023-04-17 18:25:56 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-	/**
-	 * struct dog - collection of related members
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
+/**
+ * struct dog - collection of related members
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 
 	struct dog
 	{
 		char *name;
-		float age;
+		float *age;
 		char *owner;
 	};
 
