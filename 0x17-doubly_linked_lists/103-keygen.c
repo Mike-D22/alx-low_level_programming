490eceedc601bde5eba7eea326c247187be1b3aa
Mike-D22 <qamp26@gmail.com>
2023-08-18 18:45:16 -0700

    crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
+	unsigned int z, b;
 	size_t len, add;
 	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
@@ -24,21 +24,21 @@ int main(int argc, char *argv[])
 	}
 	len = strlen(argv[1]);
 	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
+	for (z = 0, add = 0; z < len; z++)
+		add += argv[1][z];
 	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
+	for (z = 0, b = 1; z < len; z++)
+		b *= argv[1][z];
 	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
+	for (b = argv[1][0], z = 0; z < len; z++)
+		if ((char)b <= argv[1][z])
+			b = argv[1][z];
 	srand(b ^ 14);
 	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
+	for (b = 0, z = 0; z < len; z++)
+		b += argv[1][z] * argv[1][z];
 	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+	for (b = 0, z = 0; (char)z < argv[1][0]; z++)
 		b = rand();
 	p[5] = l[(b ^ 229) & 63];
 	printf("%s\n", p);
